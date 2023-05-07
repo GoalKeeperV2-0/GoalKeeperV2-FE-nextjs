7318a8599cdac7d92d0c6f702e88fed1fecb6fe0
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-05-07 21:10:04 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,31 @@
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
 import HomeScreen from '@/app.features/Home/screens/HomeScreen';
 import { useCertList } from '@/app.modules/hooks/useCertList';
+import axios from 'axios';
 
-const Home: NextPage = () => {
-	const { data: certs } = useCertList(0);
-	return (
-		<>
-			<HomeScreen
-				certs={certs?.certificationResponses?.content?.slice(0, 6)}
-				alreadyVerification={certs?.alreadyVerification}
-			/>
-		</>
-	);
+interface Props {
+	data: any;
+}
+export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
+	console.log(ctx.req.headers.cookie);
+	/*const url = 'https://api.goalkeeper.co.kr/api/certifications?page=0';
+	const response = await axios.get(url);
+	const data = response.data;*/
+	return {
+		props: { data: 12 }, // will be passed to the page component as props
+	};
+};
+const Home: NextPage<Props> = ({ data: certs }) => {
+	// const { data: certs } = useCertList(0);
+	console.dir(certs);
+	return <div />;
 };
 
 export default Home;
+/*
+
+<HomeScreen
+				certs={certs?.certificationResponses?.content?.slice(0, 6)}
+				alreadyVerification={certs?.alreadyVerification}
+			/>
+*/
