c0b8ee9a1ddfe2ebfea166c8dccb79680d7f27f9
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-05-10 01:21:38 +0900

    feat : 메인페이지 ssr 로드

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,27 +4,25 @@ import { useCertList } from '@/app.modules/hooks/useCertList';
 import axios from 'axios';
 
 interface Props {
-	data: any;
+	serverData?: any;
 }
 export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
-	console.log(ctx.req.headers.cookie);
-	/*const url = 'https://api.goalkeeper.co.kr/api/certifications?page=0';
-	const response = await axios.get(url);
-	const data = response.data;*/
-	return {
-		props: { data: 12 }, // will be passed to the page component as props
-	};
+	console.log(ctx.req.cookies.GAT);
+	const url = 'https://api.goalkeeper.co.kr/api/certifications?page=0';
+	const token = ctx.req.cookies.GAT;
+	const headers = { Authorization: `Bearer ${token}` }; // 헤더 설정
+	const response = await axios.get(url, { headers });
+	const data = response.data;
+	return { props: { serverData: data } };
 };
-const Home: NextPage<Props> = ({ data: certs }) => {
+const Home: NextPage<Props> = ({ serverData }: Props) => {
 	// const { data: certs } = useCertList(0);
-	console.dir(certs);
-	return <div />;
-
+	console.dir(serverData?.data?.certificationResponses?.content);
+	return (
+		<HomeScreen
+			certs={serverData?.data?.certificationResponses?.content?.slice(0, 6)}
+			alreadyVerification={serverData?.data?.alreadyVerification}
+		/>
+	);
+};
 export default Home;
-/*
-
-<HomeScreen
-				certs={certs?.certificationResponses?.content?.slice(0, 6)}
-				alreadyVerification={certs?.alreadyVerification}
-			/>
-*/
